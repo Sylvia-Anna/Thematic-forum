ac2f7ac7078f7740fe623b81217a2e525c9b4ec4
Sylvia-Anna <80194207+Sylvia-Anna@users.noreply.github.com>
2024-03-15 12:57:06 +0200

    Добавление файлов
    Создан dockerfiile + requirments.txt

---
+++ front/thematic_forum/src/components/FilterBar.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles(theme => ({
+    navbar: {
+      borderRadius: '5px',
+      backgroundColor: 'red',
+    },
+  }));
+
+
+function FilterBar() {
+    return (
+      
+    )
+}
