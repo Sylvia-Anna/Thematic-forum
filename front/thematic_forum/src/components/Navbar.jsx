df11af2d757dfad93231a15d70a704221433a6f8
Sylvia-Anna <80194207+Sylvia-Anna@users.noreply.github.com>
2024-03-16 23:30:12 +0200

    Небольшая доработка navbar

--- front/thematic_forum/src/components/Navbar.jsx
+++ front/thematic_forum/src/components/Navbar.jsx
@@ -14,6 +14,7 @@ import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import CssBaseline from '@mui/material/CssBaseline';
 import { makeStyles } from '@mui/styles';
+import Link from '@mui/material/Link';
 
 
 const pages = ['Products', 'Pricing', 'Blog'];
@@ -138,33 +139,10 @@ function Navbar() {
             </Box>
 
             <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                </IconButton>
-                </Tooltip>
-                <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-                >
-                {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                    </MenuItem>
-                ))}
-                </Menu>
+                <Button
+                    sx={{ my: 2, color: 'white', display: 'block' }}>
+                    Login
+                </Button>
             </Box>
             </Toolbar>
         </Container>
